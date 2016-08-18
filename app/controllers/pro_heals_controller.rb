351688f1b2a91db30c2d177c20780220f7dbdf7f
valentincranier <crasnier.val@gmail.com>
2016-08-18 16:06:07 +0200

    Add button delete

--- app/controllers/pro_heals_controller.rb
+++ app/controllers/pro_heals_controller.rb
@@ -1,26 +1,32 @@
 class ProHealsController < ApplicationController
-  def index
-    @pro_heal = ProHeal.all
-  end
+    def index
+        @pro_heal = ProHeal.all
+    end
 
-  def new
-    @pro_heal = ProHeal.new
-  end
+    def new
+        @pro_heal = ProHeal.new
+    end
 
-  def create
-    pro_heal = ProHeal.new(params_pro_heal)
-    pro_heal.avatar = params[:file]
-    pro_heal.save
-    redirect_to root_path
-  end
+    def create
+        pro_heal = ProHeal.new(params_pro_heal)
+        pro_heal.avatar = params[:file]
+        pro_heal.save
+        redirect_to root_path
+    end
 
-  def show
-    @pro_heal = ProHeal.find(params[:id])
-  end
+    def show
+        @pro_heal = ProHeal.find(params[:id])
+    end
 
-  private
+    def destroy
+      pro_heal = ProHeal.find(params[:id])
+      pro_heal.delete
+      redirect_to root_path
+    end
 
-  def params_pro_heal
-      params.require(:pro_heal).permit(:name, :surname, :phone, :location, :job, :city, :postal_code, :avatar)
-  end
+    private
+
+    def params_pro_heal
+        params.require(:pro_heal).permit(:name, :surname, :phone, :location, :job, :city, :postal_code, :avatar)
+    end
 end
